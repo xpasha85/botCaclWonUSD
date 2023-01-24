519e11dd481df7d48224eec02c4d8bd625098abf
xpasha85 <xpasha85@gmail.com>
2023-01-24 21:03:18 +1000

    Change max usd sum, komision usd and won. Fiz some bugs

--- bot.py
+++ bot.py
@@ -14,34 +14,47 @@ svyzki_dict = {}
 client = telebot.TeleBot('5630714715:AAEyHjpFNHdeHFTu-h5U3-JMSTs5BzMinEA')
 
 
-def usd_krw_api() -> str:
+def usd_krw_api() -> float:
     url = "https://openexchangerates.org/api/latest.json?app_id=b3aec8e91a634482b797a7e29bbd38f0&" \
           "symbols=KRW&prettyprint=true"
     headers = {"Accept": "application/json"}
     response = requests.get(url, headers=headers).json()
     usdwon = response['rates']['KRW']
-    return usdwon
+    return int(usdwon)
+
+
+def get_courses_from_table():
+    table = Table('keySp0t9lDFoYBbEl', 'appXujWj3BOKy9GDm', 'USDKRW')
+    last_record = table.first()
+    usd = last_record['fields']['USD']
+    krw = last_record['fields']['KRW']
+    return usd, krw
+
+
+def max_summ_usd(usd):
+    return round(2_500_000 / usd - 200, -2)
 
 
 @client.message_handler(commands=["run_auto"])
 def run_auto(message):
     chat_id = message.chat.id
-    table = Table('keySp0t9lDFoYBbEl', 'appXujWj3BOKy9GDm', 'USDKRW')
-    last_record = table.first()
-    usd = last_record['fields']['USD']
-    krw = last_record['fields']['KRW']
-    usdwon = int(usd_krw_api()) - 10
+    usd, krw = get_courses_from_table()
+    usdwon = usd_krw_api() - 10
     client.send_message(message.chat.id, 'Щас все посчитаем!!!')
     client.send_message(message.chat.id, f'Для расчета будут использованые следующие курсы: \n'
                                          f'Курс 🇺🇸 (1$) - {str(usd)} руб.\n'
                                          f'Курс 🇰🇷 (1000₩) - {str(krw)} руб.\n'
-                                         f'Курс USDKWR (XE 1$) - {str(usdwon)}₩')
-    usd40 = int(round((40000 + 200) * usd, -3))  # 40.000$ если купить в ББР РУБ
+                                         f'Курс USDKWR (XE 1$) - {str(usdwon)}₩\n'
+                                         f'Расчеты произведены на 2.500.000 руб.')
+    mx_usd = max_summ_usd(usd)  # максимальная сумма на 2500000 руб.
+    mx_usd_txt = '{0:,}'.format(mx_usd).replace(',', '.')
+    komission = mx_usd * 0.005  # комиссия на эту сумму в ББР
+    usd40 = int(round((mx_usd + komission) * usd, -3))  # mx_usd если купить в ББР РУБ
     usd40_txt = '{0:,}'.format(usd40).replace(',', '.')  # форматированное представление рублей
-    get_won_kor = round(40000 * usdwon, -3)  # примерное количесво ВОН по XE курсу
+    get_won_kor = round(mx_usd * usdwon, -3)  # примерное количесво ВОН по XE курсу
     won_kor_txt = '{0:,}'.format(get_won_kor).replace(',', '.')  # форматированное представление ВОН
     prim_rub_won = int(
-        round((get_won_kor + 35000) * (krw / 1000), -3))  # полученое кол-во вон купить в рублях в Приморье
+        round((get_won_kor + 80000) * (krw / 1000), -3))  # полученое кол-во вон купить в рублях в Приморье
     prim_rub_txt = '{0:,}'.format(prim_rub_won).replace(',', '.')
     diff = usd40 - prim_rub_won  # разница в рублях в банках
     diff_frm = '{0:,}'.format(abs(diff)).replace(',', '.')
@@ -49,7 +62,7 @@ def run_auto(message):
         diff_txt = f'Выгоднее в 🇰🇷 на {diff_frm}руб.'
     else:
         diff_txt = f'Выгоднее в 🇺🇸 на {diff_frm}руб.'
-    text = f'40000$ в ББР - примерно {usd40_txt}руб.\n' \
+    text = f'{mx_usd_txt}$ в ББР - примерно {usd40_txt}руб.\n' \
            f'В Корее получат примерно {won_kor_txt}₩\n' \
            f'☝ эта же сумма в Приморье - {prim_rub_txt}руб.\n\n' \
            f'Итого: {diff_txt}'
@@ -87,24 +100,30 @@ def calc_unswer(message):
         svyazka = svyzki_dict[user_id]
         svyazka.won = float(message.text)
         client.send_message(message.chat.id, 'Щас все посчитаем!')
-        usdwon = usd_krw_api()  # курс обмена доллар-вона по API
-        usd40 = int(round((40000 + 200) * svyazka.usd, -3))  # 40.000$ если купить в ББР  РУБ
+        usdwon = usd_krw_api() - 10  # курс обмена доллар-вона по API
+        mx_usd = max_summ_usd(svyazka.usd)  # максимальная сумма на 2500000 руб.
+        mx_usd_txt = '{0:,}'.format(mx_usd).replace(',', '.')
+        komission = mx_usd * 0.005  # комиссия на эту сумму в ББР
+        usd40 = int(round((mx_usd + komission) * svyazka.usd, -3))  # mx_usd если купить в ББР РУБ
+        #  usd40 = int(round((40000 + 200) * svyazka.usd, -3))  # 40.000$ если купить в ББР  РУБ
         usd40_txt = '{0:,}'.format(usd40).replace(',', '.')  # форматированное представление рублей
-        get_won_kor = round(40000 * (int(usdwon) - 10), -3) # примерное количесво ВОН по XE курсу
+        get_won_kor = round(mx_usd * usdwon, -3)  # примерное количесво ВОН по XE курсу
         won_kor_txt = '{0:,}'.format(get_won_kor).replace(',', '.')  # форматированное представление ВОН
-        prim_rub_won = int(round((get_won_kor + 35000) * (svyazka.won / 1000), -3)) # полученое кол-во вон купить в рублях в Приморье
+        prim_rub_won = int(
+            round((get_won_kor + 80000) * (svyazka.won / 1000), -3))  # полученое кол-во вон купить в рублях в Приморье
         prim_rub_txt = '{0:,}'.format(prim_rub_won).replace(',', '.')
-        diff = usd40 - prim_rub_won # разница в рублях в банках
+        diff = usd40 - prim_rub_won  # разница в рублях в банках
         diff_frm = '{0:,}'.format(abs(diff)).replace(',', '.')
         if diff >= 0:
             diff_txt = f'Выгоднее в 🇰🇷 на {diff_frm}руб.'
         else:
             diff_txt = f'Выгоднее в 🇺🇸 на {diff_frm}руб.'
-        text = f'Расчеты произведены исходя из курсов валют:\n' \
+        text = f'Для расчета будут использованые следующие курсы:\n' \
                f'Курс доллара 🇺🇸: {svyazka.usd}руб.\n' \
                f'Курс воны 🇰🇷 (1000): {svyazka.won}руб.\n' \
-               f'Курс USDKRW (XE) 1$: {round(float(usdwon))}₩\n' \
-               f'40000$ в ББР - примерно {usd40_txt}руб.\n' \
+               f'Курс USDKRW (XE) 1$: {usdwon}₩\n' \
+               f'Расчеты произведены на 2.500.000 руб.\n' \
+               f'{mx_usd_txt}$ в ББР - примерно {usd40_txt}руб.\n' \
                f'В Корее получат примерно {won_kor_txt}₩\n' \
                f'☝ эта же сумма в Приморее - {prim_rub_txt}руб.\n\n' \
                f'Итого: {diff_txt}'
